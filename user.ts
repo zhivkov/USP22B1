c428cfabe5d3f0c77a653384fdac127526b1fe26
Georgi Valkov <34381381+DJBYFFYBASS@users.noreply.github.com>
2022-03-17 17:59:39 +0200

    Hello

--- user.ts
+++ user.ts
@@ -1,6 +1,6 @@
 export interface User {
-    name: string;
-    age: number;
-    isMarried: boolean;
+    nameIsFalse: string;
+    age12: number;
+    isMarriedNEVER: boolean;
     children: any[];
 }
